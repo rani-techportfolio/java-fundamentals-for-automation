148ae3afbaef296ba7e2e5655f8c22e83881b658
rani-techportfolio <160572857+rani-techportfolio@users.noreply.github.com>
2026-07-06 13:07:36 +0530

    Added program to find biggest of three numbers using conditional logic

---
+++ src/javabasics/conditionallogic/BiggestOfThree.java
@@ -0,0 +1,34 @@
+package javabasics.conditionallogic;
+
+import java.util.Scanner;
+
+public class BiggestOfThree {
+
+	public static void main(String[] args) {
+		
+		int num1, num2, num3;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the first number:  ");
+		num1 = sc.nextInt();
+		
+		System.out.println("Enter the second number:  ");
+		num2 = sc.nextInt();
+		
+		System.out.println("Enter the third number:  ");
+		num3 = sc.nextInt();
+		
+		if(num1>=num2 && num1>=num3) 
+			System.out.println(num1 +" is greatest");
+
+		else if(num2>=num1 && num2>=num3) 
+			System.out.println(num2 +" is greatest");
+		
+		else
+			System.out.println(num3 +" is greatest");
+		
+		sc.close();
+	}
+
+}
